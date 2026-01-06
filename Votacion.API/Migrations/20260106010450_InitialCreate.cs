fd8bad8ab3b2e13c7b1765624f23da7e2f8f65c3
alejo10005 <alejonavarrete05@icloud.com>
2026-01-05 20:25:53 -0500

    creado la migracion y cargada a la base de datos

---
+++ Votacion.API/Migrations/20260106010450_InitialCreate.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Votacion.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Elecciones",
+                columns: table => new
+                {
+                    EleccionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Descripcion = table.Column<string>(type: "text", nullable: false),
+                    Tipo = table.Column<int>(type: "integer", nullable: false),
+                    FechaInicio = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Cerrada = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elecciones", x => x.EleccionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NombreCompleto = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Rol = table.Column<int>(type: "integer", nullable: false),
+                    Activo = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candidatos",
+                columns: table => new
+                {
+                    CandidatoId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    EleccionId = table.Column<int>(type: "integer", nullable: false),
+                    Propuesta = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidatos", x => x.CandidatoId);
+                    table.ForeignKey(
+                        name: "FK_Candidatos_Elecciones_EleccionId",
+                        column: x => x.EleccionId,
+                        principalTable: "Elecciones",
+                        principalColumn: "EleccionId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Candidatos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "UsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TokenSesions",
+                columns: table => new
+                {
+                    TokenSesionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    Expira = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TokenSesions", x => x.TokenSesionId);
+                    table.ForeignKey(
+                        name: "FK_TokenSesions_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "UsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Votos",
+                columns: table => new
+                {
+                    VotoId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EleccionId = table.Column<int>(type: "integer", nullable: false),
+                    CandidatoId = table.Column<int>(type: "integer", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HashVotante = table.Column<string>(type: "text", nullable: false),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Votos", x => x.VotoId);
+                    table.ForeignKey(
+                        name: "FK_Votos_Candidatos_CandidatoId",
+                        column: x => x.CandidatoId,
+                        principalTable: "Candidatos",
+                        principalColumn: "CandidatoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Votos_Elecciones_EleccionId",
+                        column: x => x.EleccionId,
+                        principalTable: "Elecciones",
+                        principalColumn: "EleccionId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Votos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "UsuarioId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidatos_EleccionId",
+                table: "Candidatos",
+                column: "EleccionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidatos_UsuarioId",
+                table: "Candidatos",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenSesions_UsuarioId",
+                table: "TokenSesions",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votos_CandidatoId",
+                table: "Votos",
+                column: "CandidatoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votos_EleccionId",
+                table: "Votos",
+                column: "EleccionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votos_UsuarioId",
+                table: "Votos",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TokenSesions");
+
+            migrationBuilder.DropTable(
+                name: "Votos");
+
+            migrationBuilder.DropTable(
+                name: "Candidatos");
+
+            migrationBuilder.DropTable(
+                name: "Elecciones");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
--- Votacion.API/Program.cs
+++ Votacion.API/Program.cs
@@ -1,27 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<VotacionAPIContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("VotacionAPIContext") ?? throw new InvalidOperationException("Connection string 'VotacionAPIContext' not found.")));
 
-// Add services to the container.
-
+// ===============================
+// Servicios
+// ===============================
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<VotacionAPIContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("VotacionAPIContext")
+        ?? throw new InvalidOperationException("Connection string not found.")
+    )
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// ===============================
+// Middleware
+// ===============================
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
