2a2e93306d4914537ae50446288669a9be4d2a7e
alejo10005 <alejonavarrete05@icloud.com>
2026-01-06 21:19:24 -0500

    creado la migracion y se actualizo la base de datos

---
+++ Votacion.API/Migrations/20260107021546_AddUsuarioToVoto.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Votacion.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsuarioToVoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votos_Usuarios_UsuarioId",
+                table: "Votos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Votos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votos_Usuarios_UsuarioId",
+                table: "Votos",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "UsuarioId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votos_Usuarios_UsuarioId",
+                table: "Votos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Votos",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votos_Usuarios_UsuarioId",
+                table: "Votos",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "UsuarioId");
+        }
+    }
+}
